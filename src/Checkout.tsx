c8a6ab5f90b36e9304a6d6a35d5c4fe99233c15a
Niki-van <90352830+niki-van13@users.noreply.github.com>
2022-05-27 11:21:26 +0200

    Update Checkout.tsx

--- src/Checkout.tsx
+++ src/Checkout.tsx
@@ -1,13 +1,1 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './Checkout.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-   
-  )
-}
-
-export default App
