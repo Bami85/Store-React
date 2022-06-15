00bdff8ba7c4ca308d2e01f5da45e592b5e31954
Niki-van <hanko.vangjeli@studio.unibo.it>
2022-06-15 13:10:01 +0200

    new-footer

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,35 +1,84 @@
-// src/componetns/Footer.tsx
+import Container from '@mui/material/Container';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import CallIcon from '@mui/icons-material/Call';
+import TwitterIcon from '@mui/icons-material/Twitter'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
+import LinkedInIcon from '@mui/icons-material/LinkedIn'
+import GitHubIcon from '@mui/icons-material/GitHub';
 
-import React, { FC, ReactElement } from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
-
-export const Footer: FC = (): ReactElement => {
+export default function Footer() {
   return (
-    <Box
-      sx={{
-        width: "100%",
-        height: "auto",
-        backgroundColor: "secondary.main",
-        paddingTop: "1rem",
-        paddingBottom: "1rem",
-      }}
-    >
-      <Container maxWidth="lg">
-        <Grid container direction="column" alignItems="center">
-          <Grid item xs={12}>
-            <Typography color="black" variant="h5">
-              React Starter App
-            </Typography>
+    <footer>
+      <Box
+        px={{ xs: 2, sm: 6}}
+        py={{ xs: 2, sm: 6 }}
+        bgcolor="#999999"
+        color="white"
+      >
+        <Container maxWidth="lg">
+          <Grid container spacing={5}>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Help</Box>
+              <Box>
+             
+                  Contact
+              
+              </Box>
+              <Box>
+               
+                  Support
+          
+              </Box>
+              <Box>
+               
+                  Privacy
+             
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}>Contact</Box>
+              <Box>
+            
+               <LocationOnIcon/> Linneplatsen 10 412 11 Gothenburg
+             
+              </Box>
+              <Box>
+               
+                 < CallIcon/> +046 345234523
+         
+              </Box>
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Box borderBottom={1}> Follow Us</Box>
+              <Box>
+               
+                
+              </Box>
+              <Box>
+               
+              <FacebookIcon/>           
+            
+<TwitterIcon/>
+          
+<InstagramIcon/>
+    
+<LinkedInIcon/>
+            
+<GitHubIcon/>
+            
+              </Box>
+              
+            </Grid>
           </Grid>
-          <Grid item xs={12}>
-            <Typography color="textSecondary" variant="subtitle1">
-              {`${new Date().getFullYear()} | React | Material UI | React Router`}
-            </Typography>
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
+          <Box textAlign="center" pt={{ xs: 5, sm: 3 }} pb={{ xs: 5, sm: 0 }}>
+            Snow White &reg; {new Date().getFullYear()}
+          </Box>
+        </Container>
+      </Box>
+    </footer>
   );
-};
-
-export default Footer;
+}
